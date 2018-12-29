de6906d4920337b44f732b36b4bd3aa575c404f8
mandy kh@ <mandy@example.com>
2018-12-29 00:26:43 +0000

    use the async pipe

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -9,10 +10,10 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  user: firebase.User;
+  user$: Observable<firebase.User>;
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(user => this.user = user);
+    this.user$ = afAuth.authState;
    }
 
   logout(){
