5c9ddc54f6db67682244c9887a11ec70cc54b646
Mandana <mandanakhademi88@gmail.com>
2019-01-02 23:14:58 +0000

    update category service to get the key

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
+  documentToDomainObject = _ => {
+    const object = _.payload.val();
+    object.key = _.payload.key;
+    return object;
+  }
+
   constructor(private db: AngularFireDatabase) { }
 
   getCategories(){
-    return this.db.list('/categories', ref => ref.orderByChild('name')).valueChanges();
+    return this.db.list('/categories', ref => ref.orderByChild('name')).snapshotChanges()
+    .pipe(map(actions => actions.map(this.documentToDomainObject)));
   }
 }
