aab77f063dfec6292b4bb0f17748a58eed2b5fe2
Mandana <mandanakhademi88@gmail.com>
2019-01-09 00:44:16 +0000

    refactore the shopping cart and add a product to the shopping cart

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -14,17 +14,7 @@ export class ProductCardComponent {
   constructor(private cartService: ShoppingCartService) { }
 
   addToCart( product: Product){
-    let cartId = localStorage.getItem('cartId');
-    if (!cartId){
-      this.cartService.create().then(result =>{
-        localStorage.setItem('cartId', result.key);
-
-        //Add product to shopping cart
-      });      
-
-    }else {
-      //Add product to shopping cart
-    }
+    this.cartService.addToCart(product);
   } 
 
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Product } from './models/product';
+import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +15,40 @@ export class ShoppingCartService {
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     });
+  }
+
+  private getCart(cartId: string){
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+
+  private async getOrCreateCartId(){
+    let cartId = localStorage.getItem('cartId');
+    if (cartId) return cartId;   
+    
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;     
+  }
+
+
+
+  async addToCart( product: Product){
+    let cartId = await this.getOrCreateCartId();
+    let itemObject = this.db.object('/shopping-carts/' + cartId + '/items/' + product.key);
+    itemObject.valueChanges().pipe(take<any>(1)).subscribe(item => {
+      if (item) itemObject.update({ quantity: item.quantity + 1});
+      else itemObject.set({ product: product, quantity: 1});
+
+      // let itemPromise = () => itemObject.valueChanges().pipe(take<any>(1)).toPromise();
+      // let item = await itemPromise();
+      // if (item) itemObject.update({ quantity: item.quantity + 1});
+      // else itemObject.set({ product: product, quantity: 1});
+
+    });
+    
+    
+      
+    
   }
 }
