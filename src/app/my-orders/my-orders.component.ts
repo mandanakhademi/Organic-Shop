0c2d5b7762394c3c2c7819397e7b8ab986d8cb5e
Mandana <mandanakhademi88@gmail.com>
2019-01-17 01:12:11 +0000

    display the orders

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { OrderService } from '../order.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-my-orders',
+  selector: 'my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent {
+  orders$;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService) {
+      this.orders$ = this.authService.user$.pipe(switchMap(u => this.orderService.getOrdersByUser(u.uid).valueChanges()));
+       
 
-  ngOnInit() {
   }
-
+  
 }
--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -13,6 +13,15 @@ export class OrderService {
     let result = await this.db.list('/orders').push(order);
     this.shoppingCartService.clearCart();
     return result;
-
   }
+
+  getOrders(){
+    return this.db.list('/orders');
+  }
+
+  getOrdersByUser(userId: string) {
+    return this.db.list('/orders', ref => 
+      ref.orderByChild('userId').equalTo(userId));
+  }
+      
 }
